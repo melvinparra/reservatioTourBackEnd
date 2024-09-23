cdd86550ba068cc0549d08efb10bb0a091c872d1
melvinparra <135729962+melvinparra@users.noreply.github.com>
2024-09-23 12:47:43 -0400

    config

--- config/firebase.js
+++ config/firebase.js
@@ -9,18 +9,3 @@ admin.initializeApp({
 const db = admin.firestore();
 
 module.exports = db;
-
-// const admin = require('firebase-admin');
-// const path = require('path');
-
-// // Ruta absoluta hacia el archivo de credenciales JSON en la raíz del disco C
-// const serviceAccount = require('C:/FbaseReservationTour.json');
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: 'https://reservationtour-38ed1.firebaseio.com' // Asegúrate de que esta URL sea correcta
-// });
-
-// const db = admin.firestore();
-
-// module.exports = db;
